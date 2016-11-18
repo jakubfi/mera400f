1337a27012f24d0e2f8c45b324e0e15da851345a
Jakub Filipowicz <jakubf@gmail.com>
2016-11-18 14:23:46 +0100

    synthesis ramstyle = "M4K"

--- src/mera400f.v
+++ src/mera400f.v
@@ -1,14 +1,14 @@
-module mera400f(input dummy);
+module mera400f(
+	input [15:0] w,
+	output [15:0] l,
+	input czytrn,
+	input piszrn,
+	input czytrw,
+	input piszrw,
+	input ra, rb
+);
 
-	wire [15:0] w;
-	wire [15:0] l;
-	wire czytrn;
-	wire piszrn;
-	wire czytrw;
-	wire piszrw;
-	wire ra, rb;
-
-	regs u_regs(.w(w), .l(l), .czytrn(czytrn), .piszrn(piszrn), .czytrw(czytrw), .piszrw(piszrw), .ra(ra), .rb(rb), .rc(0));
+	regs u_regs(.w(w), .l(l), .czytrn(czytrn), .piszrn(piszrn), .czytrw(czytrw), .piszrw(piszrw), .ra(ra), .rb(rb));
 
 endmodule
 
--- src/regs.v
+++ src/regs.v
@@ -6,21 +6,23 @@
 	pages:		2-59, 2-60, 2-61, 2-62
 */
 
+/* synthesis ramstyle = "M4K" */
+
 module regs (
 	output [15:0] l,
 	input [15:0] w,
-	input ra, rb, rc, // rc is unused here. Added only to force auto-M4K storage on Altera devices
+	input ra, rb,
 	input piszrn, czytrn,
 	input piszrw, czytrw
 );
 
-	reg [3:0] read_add;
+	reg [2:0] read_add;
 	reg [15:0] mem [15:0];
 
 	wire wr = piszrn | piszrw;
 	wire rd = czytrn | czytrw;
 	wire h = czytrw | piszrw;
-	wire [3:0] addr = {h, ra, rb, rc};
+	wire [2:0] addr = {h, ra, rb};
 	wire strobe = wr | rd;
 
 	always @ (posedge strobe) 
