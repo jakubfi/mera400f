dafc93ce6038471906f980cdc9e04517bad1545d
Jakub Filipowicz <jakubf@gmail.com>
2017-03-08 12:55:36 +0100

    dummy sram fix

--- src/mem_dummy_sram.v
+++ src/mem_dummy_sram.v
@@ -10,28 +10,38 @@ module mem_dummy_sram(
 	output ok_
 );
 
+	assign SRAM_CE = 0;
+	assign SRAM_UB = 0;
+	assign SRAM_LB = 0;
+
 	assign SRAM_A[17:0] = { 2'b00, ~ad_[0:15] };
-	assign SRAM_CE = w_ & r_;
-	assign SRAM_OE = r_;
-	assign SRAM_WE = w_;
-	assign SRAM_UB = w_ & r_;
-	assign SRAM_LB = w_ & r_;
 	assign SRAM_D = ~w_ ? ~rdt_[0:15] : 16'hzzzz;
 	assign ddt_ = ~r_ ? ~SRAM_D[15:0] : 16'hffff;
 
-	wire ok_dly;
-	dly #(.ticks(2'd2)) DLY_OK(
+	wire we;
+	assign SRAM_WE = ~we;
+	univib #(.ticks(1'd1)) WE_VIB(
 		.clk(clk),
-		.i(~SRAM_CE),
-		.o(ok_dly)
+		.a_(w_),
+		.b(1'b1),
+		.q(we)
+	);
+
+	wire re;
+	assign SRAM_OE = ~re;
+	univib #(.ticks(1'd1)) RE_VIB(
+		.clk(clk),
+		.a_(r_),
+		.b(1'b1),
+		.q(re)
 	);
 
 	wire ok;
 	assign ok_ = ~ok;
-	univib #(.ticks(2'd2)) DLY_VIB(
+	univib #(.ticks(1'd1)) DLY_VIB(
 		.clk(clk),
 		.a_(1'b0),
-		.b(ok_dly),
+		.b(re | we),
 		.q(ok)
 	);
 
