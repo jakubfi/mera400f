d8492062f725e3a03efc1c7e2f2df5941fdf92c1
Jakub Filipowicz <jakubf@gmail.com>
2017-04-17 14:31:31 +0200

    AWP: fix FIC clocking

--- src/fic.v
+++ src/fic.v
@@ -14,24 +14,15 @@ module fic(
 	input [0:5] in,
 	output reg [0:5] out
 );
-/*
-	always @(posedge rab_, negedge load_, posedge cda, posedge cua_) begin
-		if (rab_) out <= 0;
-		else if (~load_) out <= in;
-		else if (cda) out <= out - 1'b1;
-		else out <= out + 1'b1;
-	end
-*/
+
 	// NOTE: Sensitivities are different for the FPGA implementation.
 	//       Idea behind it is to always be front-edge sensitive
-	wire clk = ~load_ | cda | ~cua_;
-	always @ (posedge clk, posedge rab_) begin
+	wire clk = ~load_ | ~cda | ~cua_ | rab_;
+	always @ (posedge clk) begin
 		if (rab_) out <= 0;
 		else if (~load_) out <= in;
-		else begin
-			if (cda) out <= out - 1'd1;
-			else out <= out + 1'd1;
-		end
+		else if (~cda) out <= out - 1'd1;
+		else out <= out + 1'd1;
 	end
 
 endmodule
