jakubfi/mera400f

commit bb97e4d98d7adc431e559e4fefa367ed5910b5c5

Workaround for T-M register interdependencies
T and M are clocked with different clocks derived from the same strob.
Problem is that this is enough for data to come too early or too late
and cause various misbehaviors.
To solve that strob1 (STROB*FP) has been separated from the register
enable signals and T/M registers are now driven that way.

src/fpm.v
@@ -7,6 +7,7 @@
 */
 
 module fpm(
+	input opm, opta, optb, optc,
 	input __clk,
 	// sheet 1
 	input [8:15] w,
@@ -439,10 +440,11 @@ module fpm(
 	wire M66_8 = ~(c0_eq_c1 & dw & ta);
 
 	wire t_1_ = ~t_1;
-	ffd REG_T_1(
+	ffd_ena REG_T_1(
 		.s_(1),
 		.d(M40_8),
-		.c(clockta_),
+		.c(strob_fp_),
+		.ena(opta),
 		.r_(~_0_t),
 		.q(t_1)
 	);
@@ -458,10 +460,11 @@ module fpm(
 	wire M22_8 = (~trb_ & ~t39_) | (~m0_ & ~mb) | (t_1 & f4) | (af & ~c39_ & f8_n_wdt) | (sf & f8_n_wdt & M9_6);
 
 	wire m_1_ = ~m_1;
-	ffd REG_M_1(
+	ffd_ena REG_M_1(
 			.s_(1),
 			.d(M22_8),
-			.c(~clockm),
+			.c(strob_fp_),
+			.ena(opm),
 			.r_(~_0_m),
 			.q(m_1)
 	);
@@ -472,10 +475,11 @@ module fpm(
 
 	wire ck;
 	assign ck_ = ~ck;
-	ffd REG_CK(
+	ffd_ena REG_CK(
 		.s_(M70_11),
 		.d(M77_6),
-		.c(~clockm),
+		.c(strob_fp),
+		.ena(opm),
 		.r_(~_0_m),
 		.q(ck)
 	);
@@ -487,10 +491,11 @@ module fpm(
 	wire M52_6 = ~((M13_6 & ~pm) | (M13_8 & mfwp));
 
 	wire pm;
-	ffd REG_PM(
+	ffd_ena REG_PM(
 		.s_(1),
 		.d(M52_6),
-		.c(~clockm),
+		.c(strob_fp_),
+		.ena(opm),
 		.r_(~_0_m),
 		.q(pm)
 	);

src/awp.v
@@ -53,6 +53,7 @@ module awp(
 // -----------------------------------------------------------------------
 
 wire strob2_fp, _0_f_, _0_t, lkb_, l_d_, clocktc_, clocktb_, clockta_, t_c_, fcb_, t_1_t_1, tab_, trb_, taa, cp_, frb_, p_16_, p_32_, p_40_, fab_, faa_, fra_, f5_, f6_, f2_, f4_, f13, f10_, f9, f8_, f7_, f9_ka_, scc_, pc8, _0_d, _0_m, mb, ma, clockm, zpa_, zpb_, _0_zp_, ustr0_fp, lp_;
+wire opta, optb, optc, opm;
 
 fps #(
 	.FP_STROB1_1_TICKS(FP_STROB1_1_TICKS),
@@ -65,6 +66,10 @@ fps #(
 	.FP_START_TICKS(FP_START_TICKS)
 ) FPS(
 	.__clk(__clk),
+	.opm(opm),
+	.opta(opta),
+	.optb(optb),
+	.optc(optc),
 	.mode(mode),
 	.step_(step_),
 	.strob_fp_(strob_fp_),
@@ -177,6 +182,8 @@ wire d_1, g, wdt, wt, fic_, fic, c_f, v_f, m_f, z_f, dw, ad, sd$_, mw_, dw_, af,
 fpm #(
 	.FP_FI0_TICKS(FP_FI0_TICKS)
 ) FPM(
+	.opta(opta),
+	.opm(opm),
 	.__clk(__clk),
 	.w(w),
 	.l_d_(l_d_),
@@ -293,6 +300,11 @@ fpm #(
 wire t_0_1_, t_2_7_, c0_eq_c1, c1_, fc0_, t1_, t0_eq_c0, t0_c0, t0_t1, m0_, t0_, fp0_, t_8_15_, m14_, m15_, fp16_, t16_, m32_, m38_, m39_, c39_, p32_, t39_, t_32_39_, t_16_23_, t_24_31_;
 
 fpa FPA(
+	.opta(opta),
+	.optb(optb),
+	.optc(optc),
+	.opm(opm),
+	.strob_fp_(strob_fp_),
 	.w(w),
 	.taa(taa),
 	.t_1(t_1),

src/ffd.v
@@ -1,6 +1,19 @@
 /*
 	D flip-flop
 */
+module ffd_ena(
+	input c, d, r_, s_, ena,
+	output reg q
+);
+
+	always @ (posedge c, negedge r_, negedge s_) begin
+		if (~r_) q <= 1'b0;
+		else if (~s_) q <= 1'b1;
+		else if (ena) q <= d;
+	end
+
+endmodule
+
 
 module ffd(
 	input c, d, r_, s_,

src/fpa.v
@@ -7,6 +7,8 @@
 */
 
 module fpa(
+	input opta, optb, optc, opm,
+	input strob_fp_,
 	// sheet 1
 	input [0:15] w, // also on sheets 2, 12, 13
 	input taa,
@@ -124,9 +126,9 @@ module fpa(
 
 	reg [0:39] t;
 
-	always @ (posedge clockta_, negedge _0_t_) begin
+	always @ (posedge strob_fp_, negedge _0_t_) begin
 		if (~_0_t_) t[0:15] <= 0;
-		else case ({tab_, taa_})
+		else if (opta) case ({tab_, taa_})
 			2'b00: t[0:15] <= t[0:15];
 			2'b01: t[0:15] <= {t_1, t[0:14]};
 			2'b10: t[0:15] <= {t[1:15], t[16]};
@@ -138,9 +140,9 @@ module fpa(
 	assign t_2_7_ = ~(t[2] | t[3]) & ~(t[4] | t[5]) & ~(t[6] | t[7]);
 	assign t_8_15_ = ~(t[8] | t[9]) & ~(t[10] | t[11]) & ~(t[12] | t[13]) & ~(t[14] | t[15]);
 
-	always @ (posedge clocktb_, negedge _0_t_) begin
+	always @ (posedge strob_fp_, negedge _0_t_) begin
 		if (~_0_t_) t[16:31] <= 0;
-		else case ({trb_, taa_})
+		else if (optb) case ({trb_, taa_})
 			2'b00: t[16:31] <= t[16:31];
 			2'b01: t[16:31] <= {t[15], t[16:30]};
 			2'b10: t[16:31] <= {t[17:31], t[32]};
@@ -148,9 +150,9 @@ module fpa(
 		endcase
 	end
 
-	always @ (posedge clocktc_, negedge _0_t_) begin
+	always @ (posedge strob_fp_, negedge _0_t_) begin
 		if (~_0_t_) t[32:39] <= 0;
-		else case ({trb_, taa_})
+		else if (optc) case ({trb_, taa_})
 			2'b00: t[32:39] <= t[32:39];
 			2'b01: t[32:39] <= {t[31], t[32:38]};
 			2'b10: t[32:39] <= {t[33:39], m_1};
@@ -169,9 +171,9 @@ module fpa(
 	wire mb_ /* synthesis keep */ = ~mb;
 	wire _0_m_ = ~_0_m;
 
-	always @ (posedge clockm_, negedge _0_m_) begin
+	always @ (posedge strob_fp_, negedge _0_m_) begin
 		if (~_0_m_) m[0:39] <= 0;
-		else case ({mb_, ma_})
+		else if (opm) case ({mb_, ma_})
 			2'b00: m[0:39] <= m[0:39];
 			2'b01: m[0:39] <= {m_1, m[0:38]};
 			2'b10: m[0:39] <= {m[1:31], m_32, m[33:39], m_40};

src/fps.v
@@ -34,6 +34,7 @@ module fps(
 	output clocktc_,
 	output clocktb_,
 	output clockta_,
+	output opta, optb, optc, opm,
 	output t_c_,
 	output fcb_,
 	// sheet 4
@@ -278,6 +279,12 @@ module fps(
 	wire M30_11 = ~(mw_ & lp1_);
 	wire M20_6 = ~((M30_11 & M30_6) | (f7 & ~ta_alpha_));
 
+	// WORKAROUND: opt[abc] and opm are a workaround for T and M
+	// clocks being misaligned causing various problems when
+	// values from one register are shifted into the other one.
+	assign optc = M9_3;
+	assign optb = M19_8;
+	assign opta = M19_6;
 	assign clocktc_ = ~(M9_3 & strob1);
 	assign clocktb_ = ~(M19_8 & strob1);
 	assign clockta_ = ~(strob1 & M19_6);
@@ -489,6 +496,8 @@ module fps(
 	assign mb = ~(f11_ & M18_6);
 	assign ma = ~(M28_6 & f12_);
 	assign clockm = M27_12 & strob1;
+	// WORKAROUND: for M/T registers
+	assign opm = M27_12;
 
 	// sheet 14
 

commit a589b00adaed7a31df3dd1b25d698a3332280aaa

fix REG_CK clocking bug introduced in bb97e4d98d7adc431e559e4fefa367ed5910b5c5

src/fpm.v
@@ -479,7 +479,7 @@ module fpm(
 	ffd_ena REG_CK(
 		.s_(M70_11),
 		.d(M77_6),
-		.c(strob_fp),
+		.c(strob_fp_),
 		.ena(opm),
 		.r_(~_0_m),
 		.q(ck)