c2c2a959298a3fcd0cd9d1e0159fe486882a027e
Jakub Filipowicz <jakubf@gmail.com>
2017-08-08 07:25:16 +0200

    sync DOK

--- src/dok.v
+++ src/dok.v
@@ -10,6 +10,40 @@ module dok(
 	parameter DOK_DLY_TICKS;
 	parameter DOK_TICKS;
 
+	localparam S_IDLE = 2'd0;
+	localparam S_WAIT = 2'd1;
+	localparam S_OK		= 2'd2;
+
+	reg [1:0] state;
+	reg [2:0] okcnt;
+
+	// TODO: check, simplify, make compatibile with the interface?
+
+	always @ (posedge clk_sys) begin
+		case (state)
+			S_IDLE: begin
+				if (rin) state <= S_WAIT;
+			end
+
+			S_WAIT: begin
+				if (~zw) begin	
+					state <= S_OK;
+					okcnt <= DOK_TICKS;
+				end;
+			end
+
+			S_OK: begin
+				okcnt <= okcnt - 1'b1;
+				if (okcnt == 0) state <= S_IDLE;
+			end
+
+		endcase
+	end
+
+	assign dok_trig = (state == S_WAIT);
+	assign dok = (state == S_OK) & rin;
+
+/*
 	wire rin_dly;
 	dly #(.ticks(DOK_DLY_TICKS)) DLY_DOK(
 		.clk(clk_sys),
@@ -34,6 +68,7 @@ module dok(
 	);
 
 	assign dok = rin & dok_send;
+*/
 
 endmodule
 
