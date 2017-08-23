d2a7676f98d3f8a923779bcfc80b92f65605f2c5
Jakub Filipowicz <jakubf@gmail.com>
2017-08-23 20:18:32 +0200

    AWP: really add K bus k.v module

---
+++ src/k.v
@@ -0,0 +1,21 @@
+module k(
+	input lkb,
+	input f9,
+	input [0:39] sum,
+	input [0:39] m,
+	input [0:15] w,
+	output [0:39] k
+);
+
+	always @ (*) begin
+		case ({lkb, f9})
+			2'b00: k <= sum;
+			2'b01: k <= m;
+			2'b10: k <= {w[0:15], w[0:15], w[0:7]};
+			2'b11: k <= 40'd0;
+		endcase
+	end
+
+endmodule
+
+// vim: tabstop=2 shiftwidth=2 autoindent noexpandtab
